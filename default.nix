d65114c8d9c3c13554dba6a51c1deaaa965d2e47
soup6020 <ecallaghan@protonmail.com>
2025-02-19 14:39:44 -0800

    dream2nixify

---
+++ default.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}:
+let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+
+in
+{
+  imports = [
+    dream2nix.modules.dream2nix.pip
+  ];
+  deps =
+    { nixpkgs, ... }:
+    {
+      python = nixpkgs.python3;
+    };
+
+  inherit (pyproject.project) name version;
+
+  mkDerivation = {
+    src = lib.cleanSourceWith {
+
+      src = lib.cleanSource ./.;
+      filter =
+        name: type:
+        !(builtins.any (x: x) [
+          (lib.hasSuffix ".nix" name)
+          (lib.hasPrefix "." (builtins.baseNameOf name))
+          (lib.hasSuffix "flake.lock" name)
+        ]);
+    };
+  };
+
+  buildPythonPackage = {
+    pyproject = true;
+    pythonImportsCheck = [
+      "capa"
+    ];
+  };
+
+  pip = {
+    requirementsList = pyproject.build-system.requires or [ ] ++ pyproject.project.dependencies or [ ];
+    flattenDependencies = true;
+    overrides.click = {
+      buildPythonPackage.pyproject = true;
+      mkDerivation.nativeBuildInputs = [ config.deps.python.pkgs.flit-core ];
+    };
+  };
+}
